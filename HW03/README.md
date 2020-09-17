90fd7fe7f615c15e05b5faff337925bb929a1881
anhvo <anh.vnn810@gmail.com>
2020-09-17 19:05:33 -0400

    Upload HW03

---
+++ HW03/README.md
@@ -0,0 +1,8 @@
+# CS681-HW03
+CS681 - Homework 3
+
+Student Name: Anh Vo
+
+Student Email: anh.vo003@umb.edu
+
+Student ID: 01881055
---
+++ HW03/src/edu/umb/cs681/hw03/Car.java
@@ -0,0 +1,91 @@
+package edu.umb.cs681.hw03;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Car {
+    private String make, model;
+    private int mileage, year, price, dominationCount;
+
+    public Car(String make, String model, int mileage, int year, int price) {
+        this.make = make;
+        this.model = model;
+        this.mileage = mileage;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public int getMileage() {
+        return this.mileage;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setDominationCount(List<Car> cars) {
+        int count = 0;
+        for (Car car : cars) {
+            if (!car.equals(this)) {
+                int price = car.getPrice();
+                int year = car.getYear();
+                int mileage = car.getMileage();
+
+                if (this.getYear() >= year && this.getMileage() <= mileage && this.getPrice() <= price) {
+                    if (this.getYear() > year || this.getMileage() < mileage || this.getPrice() < price) {
+                        count++;
+                    }
+                }
+            }
+        }
+        this.dominationCount = count;
+    }
+
+    public int getDominationCount() {
+        return this.dominationCount;
+    }
+
+    public static void main(String args[]) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Toyota", "SE", 190, 2018, 300));
+        cars.add(new Car("BMW", "XM6", 10, 2019, 200));
+        cars.add(new Car("Ford", "Runner", 200, 2010, 500));
+        cars.add(new Car("Mercedes", "SL550", 0, 2020, 100));
+
+        cars.forEach((Car car) -> car.setDominationCount(cars));
+
+        Integer minPrice = cars.stream().map((Car car) -> car.getPrice()).reduce(0, (result, price) -> {
+           if (result == 0) return price;
+           else if (price < result) return price;
+           else return result;
+        });
+
+        System.out.println("Min price is: " + minPrice);
+
+        Integer maxMileage = cars.stream().map((Car car) -> car.getMileage()).reduce(0, (result, mileage) -> {
+            if (result == 0) return mileage;
+            else if (mileage > result) return mileage;
+            else return result;
+        });
+
+        System.out.println("Max mileage is: " + maxMileage);
+
+        Integer numberOfCars = cars.stream().map((Car car) -> car.getMake()).reduce(0, (result, make) -> ++result, (finalResult, intermidiateResult)->finalResult);
+
+        System.out.println("Number of cars is: " + numberOfCars);
+    }
+}
