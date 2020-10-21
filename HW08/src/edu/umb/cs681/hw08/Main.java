7f64b28b5732b9613904f1e2572dfac23d3c667c
anhvo <anh.vnn810@gmail.com>
2020-10-21 13:38:34 -0400

    Upload HW08

---
+++ HW08/src/edu/umb/cs681/hw08/Main.java
@@ -0,0 +1,8 @@
+package edu.umb.cs681.hw08;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ HW08/src/edu/umb/cs681/hw08/MultiThread.java
@@ -0,0 +1,18 @@
+package edu.umb.cs681.hw08;
+
+public class MultiThread implements Runnable {
+    public void run() {
+        System.out.println(ConcurrentSingleton.getInstance());
+    }
+
+    public static void main(String[] args) {
+        MultiThread m1 = new MultiThread();
+        MultiThread m2 = new MultiThread();
+        MultiThread m3 = new MultiThread();
+        MultiThread m4 = new MultiThread();
+        new Thread(m1).start();
+        new Thread(m2).start();
+        new Thread(m3).start();
+        new Thread(m4).start();
+    }
+}
