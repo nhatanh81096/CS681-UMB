90e7fe0a2f4b1b6bbe1a0419cff4e0621d2dd5ee
anhvo <anh.vnn810@gmail.com>
2020-10-21 13:38:17 -0400

    Upload HW06

---
+++ HW06/src/edu/umb/cs681/hw06/RunnablePrimeGenerator.java
@@ -0,0 +1,32 @@
+package edu.umb.cs681.hw06;
+
+public class RunnablePrimeGenerator extends PrimeGenerator implements Runnable {
+	
+	public RunnablePrimeGenerator(long from, long to) {
+		super(from, to);
+	}
+	
+	public void run() {
+		generatePrimes();
+	}
+
+	public static void main(String[] args) {
+		RunnablePrimeGenerator gen1 = new RunnablePrimeGenerator(1L, 1000000L);
+		RunnablePrimeGenerator gen2 = new RunnablePrimeGenerator(1000000L, 2000000L);
+		Thread t1 = new Thread(gen1);
+		Thread t2 = new Thread(gen2);
+		t1.start();
+		t2.start();
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {}
+
+		gen1.getPrimes().forEach( (Long prime)->System.out.print(prime + ", ") );
+		gen2.getPrimes().forEach( (Long prime)->System.out.print(prime + ", ") );
+		
+		long primeNum = gen1.getPrimes().size() + gen2.getPrimes().size();
+		System.out.println("\n" + primeNum + " prime numbers are found in total.");
+	}
+
+}
